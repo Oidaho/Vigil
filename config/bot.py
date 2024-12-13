29ad23e364949c46b2f4005ee9fbfe0b0311ff04
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-12-13 22:34:51 +1000

    settings default values

--- config/bot.py
+++ config/bot.py
@@ -1,14 +1,12 @@
 # ./VK-Vigil/config/bot.py
 
-from typing import Optional
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BotSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="bot_")
 
-    group_id: Optional[int]
-    acces_token: Optional[str]
-    secret_key: Optional[str]
-    api_version: Optional[str] = "5.199"
+    group_id: int = 0
+    acces_token: str = "token"
+    secret_key: str = "secret"
+    api_version: str = "5.199"
--- config/database.py
+++ config/database.py
@@ -1,14 +1,12 @@
 # ./VK-Vigil/config/database.py
 
-from typing import Optional
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="db_")
 
-    dialect: Optional[str] = "sqlite"
-    user: Optional[str]
-    password: Optional[str]
-    hostname: Optional[str]
+    dialect: str = "sqlite"
+    user: str = "user"
+    password: str = "password"
+    hostname: str = "example.com"
