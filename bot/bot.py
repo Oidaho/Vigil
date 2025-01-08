ae96222550d4bbdd9ef7674bf8c04ba88208b789
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-08 22:06:52 +1000

    The functionality of setting the command prefix.

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,5 @@
-# ./VK-Vigil/bot/bot.py
+# ./Vigil/bot/bot.py
 
-import time
 from concurrent.futures import ThreadPoolExecutor
 
 from loguru import logger
@@ -9,11 +8,13 @@ from vk_api.bot_longpoll import VkBotLongPoll
 
 from config import configs
 
-from .context import Context, get_context
-from .context import BaseRouter
+from .context import Context, get_context, EventType
+from .routers import BaseRouter
 
 
 class Bot:
+    command_prefix: str = None
+
     def __init__(self, acces_token: str, api_version: str, group_id: int) -> None:
         self._make_session(acces_token, api_version)
         self._get_longpoll(group_id)
@@ -35,12 +36,6 @@ class Bot:
             group_id=group_id,
         )
 
-    def _handle_event(self, context: Context) -> None:
-        # Placeholder
-        time.sleep(3)
-
-        logger.info(f"Event {context.event_id} handled.")
-
     @property
     def api(self):
         """Returns the Vkontakte API object for implementation
@@ -51,10 +46,27 @@ class Bot:
         """
         return self.session.get_api()
 
+    def set_command_prefix(self, prefix: str) -> None:
+        """This function sets the prefix for recognizing the command.
+        This means that in case the text of the message event starts
+        with a character (or group of characters), the event will be recognized as an attempt
+        call the command. In this case, the context of the event will change slightly.
+
+        Args:
+            prefix (str): The prefix for recognizing the command.
+        """
+        self.command_prefix = prefix
+
     def run(self):
         """Launches the Bot. Starts sending requests to LPS,
         receiving and processing the latest events.
         """
+        if self.command_prefix is None:
+            self.command_prefix = "/"
+            logger.warning(
+                f"Command prefix was not set. Bot will use the standard prefix '{self.command_prefix}'."
+            )
+
         logger.info("Starting listening longpoll server.")
         recived_events = self.longpoll.listen
 
@@ -62,12 +74,9 @@ class Bot:
         for event in recived_events():
             if event:
                 logger.info("New event recived.")
-                ctx = get_context(event, self.api)
-                logger.info(f"Context: {ctx}")
                 logger.debug(event.raw)
 
-                self.thread_pool.submit(self._handle_event, ctx)
+                ctx = get_context(event, self.api, self.command_prefix)
+                logger.info(f"Context: {ctx}")
 
-    def include(self, router: BaseRouter) -> None:
-        # Sketch
-        pass
+                # self.thread_pool.submit(self._handle_event, ctx)
