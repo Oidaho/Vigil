9fafecaceed52ef14f2ba65bee3af3a5a91ca52c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-11 01:27:05 +1000

    added 'mark' command

--- routers/commands.py
+++ routers/commands.py
@@ -2,17 +2,55 @@ from bot.routers import Commands
 from bot.context import Context
 from typing import NamedTuple
 
+from bot.keyboards import Keyboard, ButtonColor
+from bot.keyboards.actions import Callback
+
 
 router = Commands()
 
 
-@router.register(name="test", args=())
-def test_command(ctx: Context, args: NamedTuple) -> None:
-    ctx.api.messages.send(peer_id=ctx.peer.id, message="Test command!", random_id=0)
-
-
-@router.register(name="test2", args=("arg1", "arg2"))
-def test2_command(ctx: Context, args: NamedTuple) -> None:
-    ctx.api.messages.send(
-        peer_id=ctx.peer.id, message=f"Test command with args <{args}>!", random_id=0
+@router.register(name="mark", args=())
+def mark_command(ctx: Context, args: NamedTuple) -> bool:
+    text = (
+        "⚠️ Вы хотите задать метку беседе? \n\n"
+        "Выберите необходимое дествие из меню ниже:"
     )
+
+    keyboard = (
+        Keyboard(inline=True, one_time=False, owner_id=ctx.user.id)
+        .add_row()
+        .add_button(
+            Callback(label="CHAT", payload={"call": "set_mark", "mark": "CHAT"}),
+            ButtonColor.POSITIVE,
+        )
+        .add_button(
+            Callback(label="LOG", payload={"call": "set_mark", "mark": "LOG"}),
+            ButtonColor.POSITIVE,
+        )
+        .add_row()
+        .add_button(
+            Callback(
+                label="Обновить данные беседы",
+                payload={"call": "update_coversation"},
+            ),
+            ButtonColor.SECONDARY,
+        )
+        .add_row()
+        .add_button(
+            Callback(label="Сбросить метку", payload={"call": "drop_mark"}),
+            ButtonColor.NEGATIVE,
+        )
+        .add_button(
+            Callback(label="Закрыть", payload={"call": "cancel"}),
+            ButtonColor.NEGATIVE,
+        )
+    )
+
+    ctx.api.messages.send(
+        peer_ids=ctx.peer.id,
+        random_id=0,
+        message=text,
+        keyboard=keyboard.json_str(),
+    )
+
+    return True
