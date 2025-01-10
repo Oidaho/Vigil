3469815f0de3131df1282fc9a810dec61521c70e
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-10 20:14:37 +1000

    Test commands

--- routers/commands.py
+++ routers/commands.py
@@ -8,4 +8,11 @@ router = Commands()
 
 @router.register(name="test", args=())
 def test_command(ctx: Context, args: NamedTuple) -> None:
-    ctx.api.messages.send(peer_id=ctx.peer.id, message="Test command!")
+    ctx.api.messages.send(peer_id=ctx.peer.id, message="Test command!", random_id=0)
+
+
+@router.register(name="test2", args=("arg1", "arg2"))
+def test2_command(ctx: Context, args: NamedTuple) -> None:
+    ctx.api.messages.send(
+        peer_id=ctx.peer.id, message=f"Test command with args <{args}>!", random_id=0
+    )
