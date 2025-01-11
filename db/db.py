b1d8d440cb7fbc868bab766fed330037fd8ac8f7
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-12 02:03:38 +1000

    The directory of the database location is now fixed within the container.

--- db/db.py
+++ db/db.py
@@ -5,20 +5,18 @@ from config import configs
 
 
 DIALECT = configs.database.dialect
-db_instance: Database
+db_instance: Database = None
 
-# TODO: Обернуть в функцию get_instance().
-# В будущем это будет необходимо для настройки веб-панели
+
 match DIALECT:
     case "sqlite":
-        db_instance = SqliteDatabase(f"{configs.project_name}.db")
+        db_instance = SqliteDatabase("/app/db/bot.sqlite3")
 
     case "postgresql":
-        dsn = "postgresql://{username}:{password}@{hostname}/{database}".format(
+        dsn = "postgresql://{username}:{password}@{hostname}/bot".format(
             username=configs.database.user,
             password=configs.database.password,
             hostname=configs.database.hostname,
-            database=configs.project_name,
         )
         db_instance = PostgresqlDatabase(database=dsn)
 
