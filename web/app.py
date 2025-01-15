e74c54994bc7b6420a08402b1e46f3f6b7dac5ee
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-16 02:52:01 +1000

    Connecting to a database in a lifespan

--- web/app.py
+++ web/app.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from routes import auth_router, pages_router
+from contextlib import asynccontextmanager
+from db import get_db_isntanse
 
-app = FastAPI(
-    title="Web-panel",
-)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db = get_db_isntanse()
+    db.connect()
+
+    yield
+
+    db.close()
+
+
+app = FastAPI(title="Web-panel", lifespan=lifespan)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth_router)
