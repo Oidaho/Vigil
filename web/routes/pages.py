7f676b2b4baab1d5e333a652f5c05c4f78d9d0f0
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-16 01:02:19 +1000

    Navigation and management pages

--- web/routes/pages.py
+++ web/routes/pages.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 
 @router.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
+def index(request: Request):
     is_authenticated, vk_id = get_auth(request=request)
 
     if not is_authenticated:
@@ -27,3 +27,83 @@ def read_root(request: Request):
     }
 
     return templates.TemplateResponse("index.html", context)
+
+
+@router.get("/conversations", response_class=HTMLResponse)
+def conversations(request: Request):
+    is_authenticated, vk_id = get_auth(request=request)
+
+    if not is_authenticated:
+        return RedirectResponse(
+            "/login",
+            status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+        )
+
+    context = {
+        "request": request,
+        "project": configs.project_name,
+        "title": "Чаты",
+        "authenticated": is_authenticated,
+    }
+
+    return templates.TemplateResponse("conversations.html", context)
+
+
+@router.get("/queue", response_class=HTMLResponse)
+def queue(request: Request):
+    is_authenticated, vk_id = get_auth(request=request)
+
+    if not is_authenticated:
+        return RedirectResponse(
+            "/login",
+            status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+        )
+
+    context = {
+        "request": request,
+        "project": configs.project_name,
+        "title": "Очередь",
+        "authenticated": is_authenticated,
+    }
+
+    return templates.TemplateResponse("queue.html", context)
+
+
+@router.get("/warns", response_class=HTMLResponse)
+def warns(request: Request):
+    is_authenticated, vk_id = get_auth(request=request)
+
+    if not is_authenticated:
+        return RedirectResponse(
+            "/login",
+            status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+        )
+
+    context = {
+        "request": request,
+        "project": configs.project_name,
+        "title": "Санкции",
+        "authenticated": is_authenticated,
+    }
+
+    return templates.TemplateResponse("warns.html", context)
+
+
+@router.get("/staff", response_class=HTMLResponse)
+def staff(request: Request):
+    is_authenticated, vk_id = get_auth(request=request)
+
+    if not is_authenticated:
+        return RedirectResponse(
+            "/login",
+            status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+        )
+
+    context = {
+        "request": request,
+        "project": configs.project_name,
+        "title": "Чаты",
+        "authenticated": is_authenticated,
+    }
+
+    return templates.TemplateResponse("staff.html", context)
