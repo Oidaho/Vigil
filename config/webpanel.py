0f54d0259706507c4b2f306f638340a5d55c2953
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-15 23:16:35 +1000

    minor fix

--- config/webpanel.py
+++ config/webpanel.py
@@ -2,19 +2,19 @@
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class JWTSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="jwt_")
 
     secret: str = "secret"
     algorithm: str = "HS512"
-    access_token_lifetime: int = 30 # minutes
-    refresh_token_lifetime: int = 7 # days
-    
+    access_token_lifetime: int = 30  # minutes
+    refresh_token_lifetime: int = 7  # days
+
 
 class WebpanelSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="web_")
 
-
     jwt: JWTSettings = JWTSettings()
-    admi_id: str = 1
+    admi_id: int = 1
     password: str = "password"
