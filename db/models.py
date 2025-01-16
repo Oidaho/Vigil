f9c8f4ce3e30ebc7ee8a8413243e9dce5370aa73
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-16 16:03:40 +1000

    fix

--- db/models.py
+++ db/models.py
@@ -15,7 +15,35 @@ from peewee import (
 from .db import db_instance
 
 
-# For storing moderated conversations
+DEFAULT_SETTINGS = [
+    {"name": "filter_video", "is_enabled": False},
+    {"name": "filter_audio", "is_enabled": False},
+    {"name": "filter_audio_message", "is_enabled": False},
+    {"name": "filter_link", "is_enabled": False},
+    {"name": "filter_poll", "is_enabled": False},
+    {"name": "filter_wall", "is_enabled": False},
+    {"name": "filter_doc", "is_enabled": False},
+    {"name": "filter_app_action", "is_enabled": False},
+    {"name": "filter_graffiti", "is_enabled": False},
+    {"name": "filter_sticker", "is_enabled": False},
+    {"name": "filter_forward", "is_enabled": False},
+    {"name": "filter_reply", "is_enabled": False},
+    {"name": "filter_geo", "is_enabled": False},
+    {"name": "check_curse_words", "is_enabled": False},
+    {"name": "check_account_age", "is_enabled": False},
+    {"name": "check_open_pm", "is_enabled": False},
+    {"name": "check_message_queue", "is_enabled": False},
+    {"name": "check_urls", "is_enabled": False},
+    {"name": "check_urls_hard", "is_enabled": False},
+]
+
+
+DEFAULT_DELAYS = [
+    {"name": "queue_mode_delay_minutes", "count": 30},
+    {"name": "account_age_days", "count": 7},
+]
+
+
 class Conversation(Model):
     peer_id = BigIntegerField(unique=True, null=False)
     name = CharField(max_length=255)
@@ -24,6 +52,25 @@ class Conversation(Model):
     class Meta:
         table_name = "conversations"
         database = db_instance
+
+    def save(self, *args, **kwargs):
+        created = not bool(self.id)
+        super().save(*args, **kwargs)
+
+        if created and self.mark != "LOG":
+            for setting_data in DEFAULT_SETTINGS:
+                Setting.create(
+                    conversation=self,
+                    name=setting_data["name"],
+                    is_enabled=setting_data["is_enabled"],
+                )
+
+            for delay_data in DEFAULT_DELAYS:
+                Delay.create(
+                    conversation=self,
+                    name=delay_data["name"],
+                    count=delay_data["count"],
+                )
 
 
 # For storing moderation staff
