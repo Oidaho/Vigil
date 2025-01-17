9d6071470a3c01d2e381d05ff685f92e8b671a64
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-18 00:43:48 +1000

    Reload parameter now depends on the enabled debug mode.

--- web/start.py
+++ web/start.py
@@ -1,8 +1,9 @@
 import uvicorn
+from config import configs
 
 
 def main() -> None:
-    uvicorn.run("app:app", host="0.0.0.0", port=7212, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=7212, reload=configs.debug_mode)
 
 
 if __name__ == "__main__":
