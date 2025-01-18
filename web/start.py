a5697f5a03b264b896d561ff5e37d32a2f0d66ff
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-18 20:16:49 +1000

    Uvicorn logging use colors

--- web/start.py
+++ web/start.py
@@ -3,7 +3,13 @@ from config import configs
 
 
 def main() -> None:
-    uvicorn.run("app:app", host="0.0.0.0", port=7212, reload=configs.debug_mode)
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=7212,
+        reload=configs.debug_mode,
+        use_colors=True,
+    )
 
 
 if __name__ == "__main__":
