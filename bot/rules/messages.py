a259eb806ec2aebe5f0579cd7874dd64d1a390bb
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-19 23:40:15 +1000

    Message handling rules

--- bot/rules/messages.py
+++ bot/rules/messages.py
@@ -1,5 +1,26 @@
 from src.routers import Rule
 from src.context import Context
 
+from db.models import Peer, Staff
 
-# Describe message routing rules here
+
+class FromChatOnly(Rule):
+    """Checks if the peer is marked with a specific label."""
+
+    def check(self, ctx: Context) -> bool:
+        peer = Peer.get_or_none(Peer.id == ctx.peer.id)
+        if peer is None or (peer.mark != "CHAT"):
+            return False
+
+        return True
+
+
+class IgnorePermitted(Rule):
+    """Checks if the user who invoked the command has the required permission level."""
+
+    def check(self, ctx: Context) -> bool:
+        user = Staff.get_or_none(Staff.id == ctx.user.id)
+        if user:
+            return False
+
+        return True
