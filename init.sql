de401f8ea9a85174e7b20c158ae5002377fe7d1f
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-01-29 00:45:03 +1000

    comments

--- init.sql
+++ init.sql
@@ -1,7 +1,15 @@
+/*
+The vigil user for the bot and web dashboard.
+You must set the same password as in `docker-compose.yml`.
+*/
 CREATE USER vigil WITH PASSWORD '...';
 CREATE DATABASE bot;
 ALTER DATABASE bot OWNER TO vigil;
 
+/*
+The nginx user for the proxy server.
+You must set the same password as in `docker-compose.yml`.
+*/
 CREATE USER nginx WITH PASSWORD '...';
 CREATE DATABASE npm;
 ALTER DATABASE npm OWNER TO nginx;
